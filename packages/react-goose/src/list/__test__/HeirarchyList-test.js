8c5a2769cd0d7cd9bdbb2f9cd20e68e0ccd950ee
Damien Clarke <dxinteractive@gmail.com>
2018-07-05 11:33:24 +1000

    Update and rename HeirachyList-test.js to HeirarchyList-test.js

--- packages/react-goose/src/list/__test__/HeirarchyList-test.js
+++ packages/react-goose/src/list/__test__/HeirarchyList-test.js
@@ -4,18 +4,18 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {shallow} from 'enzyme';
-import HeirachyList from '../HeirachyList';
+import HeirarchyList from '../HeirarchyList';
 Enzyme.configure({adapter: new Adapter()});
 
 
 test('will return a ul', (t: *): * => {
-    t.is(shallow(<HeirachyList>Foo</HeirachyList>).type(), 'ul');
+    t.is(shallow(<HeirarchyList>Foo</HeirarchyList>).type(), 'ul');
 });
 
 test('will have a className of HeirachyList', (t: *): * => {
-    t.is(shallow(<HeirachyList>Foo</HeirachyList>).prop('className'), 'HeirachyList');
+    t.is(shallow(<HeirarchyList>Foo</HeirarchyList>).prop('className'), 'HeirarchyList');
 });
 
 test('will be a spruce component', (t: *): * => {
-    t.is(HeirachyList.name, 'spruceComponent');
+    t.is(HeirarchyList.name, 'spruceComponent');
 });
